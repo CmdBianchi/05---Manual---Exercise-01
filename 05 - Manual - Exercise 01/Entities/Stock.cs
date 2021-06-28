635c868ae23005d28194ff86695e6a600cb5f8c9
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-28 17:05:34 -0300

    Commit 01

---
+++ 05 - Manual - Exercise 01/Entities/Stock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Entities {
+    class Stock {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+        public Stock() { }
+        public Stock(string name, double price, int amount) {
+            Name = name;
+            Price = price;
+            Amount = amount;
+        }
+        public double PriceAmount() {
+            return Price * Amount;
+        }
+        public void AddAmount(int amount) {
+             Amount += amount;
+        }
+        public void RemoveAmount(int amount) {
+             Amount -= amount;
+        }
+        public override string ToString() {
+            return Name + ", $ " + Price.ToString("F2") + ", " + Amount + " Unidades, TOTAL R$ " + PriceAmount();
+        }
+    }
+}
---
+++ 05 - Manual - Exercise 01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Entities;
+namespace _05___Manual___Exercise_01 {
+    class Program {
+        static void Main(string[] args) {           
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: "); string name = Console.ReadLine();
+            Console.Write("Preço: "); double price = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade no estoque: "); int amount = int.Parse(Console.ReadLine());
+            Stock stock = new Stock(name, price, amount);
+            Console.WriteLine(stock);
+            Console.WriteLine("");
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int add = int.Parse(Console.ReadLine());
+            stock.AddAmount(add);
+            Console.WriteLine(stock);
+
+            Console.Write("Digite o número de produtos a ser removido ao estoque: ");
+            int remove = int.Parse(Console.ReadLine());
+            stock.RemoveAmount(add);
+            Console.WriteLine(stock);
+        }
+    }
+}
